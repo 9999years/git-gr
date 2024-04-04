5f59559d320eb8f2ea8681c96e830caefcb0d021
Rebecca Turner <rbt@sent.as>
2024-04-03 17:39:03 -0700

    Add timestamps

--- src/change.rs
+++ src/change.rs
@@ -1,12 +1,18 @@
 use comfy_table::Attribute;
 use comfy_table::Cell;
 use comfy_table::Color;
+use miette::IntoDiagnostic;
+use serde_with::serde_as;
+use serde_with::TimestampSeconds;
+use time::macros::format_description;
+use time::OffsetDateTime;
 
 use crate::author::Author;
 use crate::change_id::ChangeId;
 use crate::change_number::ChangeNumber;
 use crate::change_status::ChangeStatus;
 
+#[serde_as]
 #[derive(serde::Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Change {
@@ -19,9 +25,10 @@ pub struct Change {
     pub url: String,
     pub hashtags: Vec<String>,
     #[allow(dead_code)]
-    created_on: u64,
-    #[allow(dead_code)]
-    last_updated: u64,
+    #[serde_as(as = "TimestampSeconds<i64>")]
+    pub created_on: OffsetDateTime,
+    #[serde_as(as = "TimestampSeconds<i64>")]
+    pub last_updated: OffsetDateTime,
     pub open: bool,
     pub status: ChangeStatus,
     #[serde(default)]
@@ -42,4 +49,43 @@ impl Change {
             }
         }
     }
+
+    pub fn last_updated_cell(&self, timestamp_format: TimestampFormat) -> miette::Result<Cell> {
+        let now = OffsetDateTime::now_local().into_diagnostic()?;
+        let now_date = now.date();
+        let date = self.last_updated.date();
+        let formatted = {
+            if now_date == date {
+                // Today.
+                let format = match timestamp_format {
+                    TimestampFormat::TwelveHour => {
+                        format_description!(
+                            "[hour padding:none repr:12]:[minute] [period case:lower]"
+                        )
+                    }
+                    TimestampFormat::TwentyFourHour => {
+                        format_description!("[hour padding:none repr:24]:[minute]")
+                    }
+                };
+                self.last_updated.format(format)
+            } else if now_date.year() == date.year() {
+                self.last_updated
+                    .format(format_description!("[month]-[day]"))
+            } else {
+                self.last_updated
+                    .format(format_description!("[year]-[month]-[day]"))
+            }
+        }
+        .into_diagnostic()?;
+        Ok(Cell::new(formatted))
+    }
+}
+
+/// Support for Europeans.
+#[derive(Debug, Clone, Copy)]
+pub enum TimestampFormat {
+    /// 12-hour time.
+    TwelveHour,
+    /// 24-hour time.
+    TwentyFourHour,
 }
