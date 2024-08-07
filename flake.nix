0697a8013132ab4d60d45d74f8069ff118baa62f
Rebecca Turner <rbt@sent.as>
2024-08-07 11:08:23 -0700

    Fix `time` compilation failure (#66)
    - Update `nixpkgs` (and use `rust-overlay` for up-to-date `rustc`)
    - Update `time` crate to fix
    https://github.com/rust-lang/rust/issues/127343
    - 
    This helps packagers upstream:
    https://github.com/NixOS/nixpkgs/issues/332957

--- flake.nix
+++ flake.nix
@@ -4,10 +4,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     systems.url = "github:nix-systems/default";
+
     crane = {
       url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
     advisory-db = {
       url = "github:rustsec/advisory-db";
       flake = false;
@@ -25,12 +34,42 @@
     nixpkgs,
     systems,
     crane,
+    rust-overlay,
     advisory-db,
   }: let
     eachSystem = nixpkgs.lib.genAttrs (import systems);
   in {
-    packages = eachSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+    _pkgs = eachSystem (
+      localSystem:
+        import nixpkgs {
+          inherit localSystem;
+          overlays = [
+            rust-overlay.overlays.default
+
+            (
+              final: prev: {
+                rustToolchain = final.pkgsBuildHost.rust-bin.stable.latest.default.override {
+                  targets =
+                    final.lib.optionals final.stdenv.targetPlatform.isDarwin [
+                      "x86_64-apple-darwin"
+                      "aarch64-apple-darwin"
+                    ]
+                    ++ final.lib.optionals final.stdenv.targetPlatform.isLinux [
+                      "x86_64-unknown-linux-musl"
+                      "aarch64-unknown-linux-musl"
+                    ];
+                  extensions = ["llvm-tools-preview"];
+                };
+
+                craneLib = (crane.mkLib final).overrideToolchain final.rustToolchain;
+              }
+            )
+          ];
+        }
+    );
+
+    packages = eachSystem (localSystem: let
+      pkgs = self._pkgs.${localSystem};
       inherit (pkgs) lib;
       packages = pkgs.callPackage ./nix/makePackages.nix {inherit inputs;};
     in
